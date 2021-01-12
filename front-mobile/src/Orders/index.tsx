40d61e9f36f2674353696dec5f2178271201b61a
fsclaudio <fsclaudio@gmail.com>
2021-01-12 18:25:16 -0300

    Add orders list

---
+++ front-mobile/src/Orders/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import {  StyleSheet,ScrollView ,Text, View,Image } from 'react-native';
+import Header from '../Header';
+import OrderCard from '../OrderCard'
+
+ function Orders() {
+     const handleOnPress = () => {
+
+     }
+  return (
+    <>
+    <Header />
+    <ScrollView style={styles.container} >
+      
+     <OrderCard />
+     <OrderCard />
+     <OrderCard />
+     <OrderCard />
+    </ScrollView>
+   
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+      container:{
+        paddingRight:'5%',
+        paddingLeft: '5%',
+
+      }
+      }
+);
+
+export default Orders;
